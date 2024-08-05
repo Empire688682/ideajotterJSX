7a427ab481f61dbf5a42c386e2d18971a6354c9f
Empire688682 <asehindej@gmail.com>
2024-08-06 00:53:01 +0100

    signupUpdated

--- FrontEnd/src/Component/SignUp/SignUp.jsx
+++ FrontEnd/src/Component/SignUp/SignUp.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const SignUp = () => {
     const [loginStage, setLogInStage] = useState("Login");
     const [errorMessage, setErrorMessage] = useState(null);
-    const { url,token } = useGlobalContex();
+    const { url, token } = useGlobalContex();
     const navigate = useNavigate()
     const [data, setData] = useState({
         name: "",
@@ -17,46 +17,48 @@ const SignUp = () => {
         pwdRepeat: ""
     });
 
-    const clearInputField = () =>{
-        setData({name: "",
+    const clearInputField = () => {
+        setData({
+            name: "",
             username: "",
             email: "",
             password: "",
-            pwdRepeat: ""})
+            pwdRepeat: ""
+        })
     };
 
-    const handleOnchange = (e) =>{
-        const {name, value} = e.target;
-        setData((prev) =>({...prev, [name]:value}));
+    const handleOnchange = (e) => {
+        const { name, value } = e.target;
+        setData((prev) => ({ ...prev, [name]: value }));
         setErrorMessage("")
     };
 
-    const handleFormSubmision = (e) =>{
+    const handleFormSubmision = (e) => {
         e.preventDefault();
         signUpLogIn()
     };
 
-    const signUpLogIn = async () =>{
+    const signUpLogIn = async () => {
         let newUrl = url;
-        if(loginStage === "Login"){
+        if (loginStage === "Login") {
             newUrl += "/api/user/login"
         }
-        else{
+        else {
             newUrl += "/api/user/register"
         }
         try {
-            const response = await axios.post(newUrl, data, {headers:{token}});
-            if(response.data.success){
+            const response = await axios.post(newUrl, data, { headers: { token } });
+            if (response.data.success) {
                 clearInputField()
-                localStorage.setItem("token",response.data.token);
-                localStorage.setItem("user",response.data.User);
+                localStorage.setItem("token", response.data.token);
+                localStorage.setItem("user", response.data.User);
                 setLogInStage("Login");
-                if(loginStage === "Login"){
+                if (loginStage === "Login") {
                     navigate("/")
                     window.location.reload()
                 }
             }
-            else{
+            else {
                 setErrorMessage(response.data.message)
             }
         } catch (error) {
@@ -68,6 +70,7 @@ const SignUp = () => {
         <div className="sign-up">
             <div className="container">
                 <h1>{loginStage === "Signup" ? "Signup" : "Login"}</h1>
+                <h4>{loginStage === "Signup" ? "" : "WELCOME BACK"}</h4>
                 <form onSubmit={handleFormSubmision}>
                     <div>
                         {loginStage === "Login" ?
@@ -91,7 +94,7 @@ const SignUp = () => {
                         }
                     </div>
                     {
-                        errorMessage? <p>{errorMessage}</p>:null
+                        errorMessage ? <p>{errorMessage}</p> : null
                     }
                     <button type="submit">
                         {loginStage === "Login" ?
@@ -100,16 +103,27 @@ const SignUp = () => {
                             "Signup"}
                     </button>
                 </form>
-                <p className='form-policy'>
-                    <input type='checkbox' required />
-                    <span>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet illo laborum accusamus modi consequuntur ipsam iusto quas saepe eveniet nulla, ipsa doloremque soluta velit minus ea rerum consequatur, vero deserunt.</span>
-                </p>
                 <div className="login-status">
                     {loginStage === "Signup" ? (
                         <p onClick={clearInputField} className='create-already'>Already have an account? <span onClick={() => setLogInStage("Login")}>Login</span></p>
                     ) : (
                         <p onClick={clearInputField} className='create-already'>Create a new account? <span onClick={() => setLogInStage("Signup")}>Click here</span></p>
                     )}
+                </div>
+                <div className="user_agreement">
+                <h3>User Agreement</h3>
+                <ul>
+                    <li><b>Account Responsibility:</b> You are responsible for all activities under your account. Keep your login credentials secure.</li>
+                    <li><b>Appropriate Use:</b> Use IdeaJotter responsibly. Do not upload harmful, offensive, or illegal content.</li>
+                    <li><b>Data Privacy:</b>Your data is important to us. We will not share your personal information without your consent, except as required by law.</li>
+                    <li><b>Modification and Termination:</b>We may modify or discontinue the platform at any time. Accounts may be suspended or terminated for violations.</li>
+                    <li><b>Limitation of Liability:</b>IdeaJotter is provided "as is." We are not liable for any damages resulting from your use of the platform.</li>
+                </ul>
+                <p>
+                    <li>I have read and agree to the User Agreement.</li>
+                    <input type="checkbox" name="" id="" />
+                    I agree to the terms and conditions
+                </p>
                 </div>
             </div>
         </div>
